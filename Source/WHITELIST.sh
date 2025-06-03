cc50b67d9bd0c8c06f9bf9c1539364c0e2245454
lucky-cry <739270050@qq.com>
2025-06-03 15:07:19 +0800

    Update WHITELIST.sh
    fix 电池优化白名单.prop 第二行不生效的bug

--- Source/WHITELIST.sh
+++ Source/WHITELIST.sh
@@ -1,5 +1,5 @@
 WHITELIST=/sdcard/Android/BATTERYOPT/电池优化白名单.prop
 
-whitelist=$(cat "$WHITELIST" | grep -v '^#' | cut -f2 -d '=')
+whitelist=$(cat "$WHITELIST" | grep -v '^#' | grep '^+' | sed 's/^+//')
 whitelist=`pm list packages -3 | sed "s/package:/-/g"`"$whitelist"
 dumpsys deviceidle whitelist $whitelist
