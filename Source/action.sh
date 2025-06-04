bc22d61ba53a0e5edf8c296749f6bc7bd0e1acda
lucky-cry <739270050@qq.com>
2025-06-04 19:05:56 +0800

    增加更新源
    增加切换更新源功能(Gitee/GitHub)

---
+++ Source/action.sh
@@ -0,0 +1,24 @@
+#!/system/bin/sh
+
+# 定义两个可选的 updateJson URL
+GITEE_URL="https://gitee.com/lucky__cat/magisk-modules/raw/Battery_whitelist/update.json"
+GITHUB_URL="https://raw.githubusercontent.com/lucky-cry/magisk-modules/Battery_whitelist/update.json"
+
+# 切换 updateJson 的函数
+switch_update_json() {
+    MODULE_PROP="/data/adb/modules/Battery_whitelist/module.prop"
+    CURRENT_URL=$(grep "updateJson=" "$MODULE_PROP" | cut -d= -f2)
+    
+    if [ "$CURRENT_URL" = "$GITEE_URL" ]; then
+        # 切换到 GitHub URL
+        sed -i "s|updateJson=.*|updateJson=$GITHUB_URL|g" "$MODULE_PROP"
+        echo "已切换到 GitHub 更新源"
+    else
+        # 切换到 Gitee URL（默认）
+        sed -i "s|updateJson=.*|updateJson=$GITEE_URL|g" "$MODULE_PROP"
+        echo "已切换到 Gitee 更新源（默认）"
+    fi
+}
+
+# 执行更新源切换
+switch_update_json
