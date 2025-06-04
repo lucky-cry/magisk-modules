6b6a167938de42487c1a60ec1236334c9a589f19
lucky-cry <739270050@qq.com>
2025-06-04 14:05:39 +0800

    Update action.sh
    增加在两个源之间切换

--- Source/action.sh
+++ Source/action.sh
@@ -1,3 +1,29 @@
+#!/system/bin/sh
+
+# 定义两个可选的 updateJson URL
+GITEE_URL="https://gitee.com/lucky__cat/magisk-modules/raw/Hcfile_sharing/update.json"
+GITHUB_URL="https://raw.githubusercontent.com/lucky-cry/magisk-modules/Hcfile_sharing/update.json"
+
+# 切换 updateJson 的函数
+switch_update_json() {
+    MODULE_PROP="/data/adb/modules/Hcfile_sharing/module.prop"
+    CURRENT_URL=$(grep "updateJson=" "$MODULE_PROP" | cut -d= -f2)
+    
+    if [ "$CURRENT_URL" = "$GITEE_URL" ]; then
+        # 切换到 GitHub URL
+        sed -i "s|updateJson=.*|updateJson=$GITHUB_URL|g" "$MODULE_PROP"
+        echo "已切换到 GitHub 更新源"
+    else
+        # 切换到 Gitee URL（默认）
+        sed -i "s|updateJson=.*|updateJson=$GITEE_URL|g" "$MODULE_PROP"
+        echo "已切换到 Gitee 更新源（默认）"
+    fi
+}
+
+# 执行更新源切换
+switch_update_json
+
+# 原有的模块更新功能
 magisk=/data/adb/magisk/busybox
 kernelsu=/data/adb/ksu/bin/busybox
 apatch=/data/adb/ap/bin/busybox
