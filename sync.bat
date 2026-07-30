fc49b1bfe350c1cc6630497379719f0a87f7f00f
lucky-cry <739270050@qq.com>
2026-07-30 23:43:23 +0800

    update 2026/07/30 周四

--- sync.bat
+++ sync.bat
@@ -1,4 +1,4 @@
-@echo off
+  @echo off
   cd /d "%~dp0"
   git config user.name "lucky-cry" >nul 2>&1
   git config user.email "739270050@qq.com" >nul 2>&1
@@ -7,6 +7,6 @@
   echo [2/3] git commit...
   git commit -m "update %date%"
   echo [3/3] git push...
-  git push origin freeze_logd_switch
+  git push origin freeze_logd_switch --force
   echo Done.
   pause
