3ffe9ef716f0266b089234c527ee2ac1da75e770
lucky-cry <739270050@qq.com>
2026-07-30 23:40:34 +0800

    update 2026/07/30 周四

---
+++ sync.bat
@@ -0,0 +1,12 @@
+@echo off
+  cd /d "%~dp0"
+  git config user.name "lucky-cry" >nul 2>&1
+  git config user.email "739270050@qq.com" >nul 2>&1
+  echo [1/3] git add...
+  git add .
+  echo [2/3] git commit...
+  git commit -m "update %date%"
+  echo [3/3] git push...
+  git push origin freeze_logd_switch
+  echo Done.
+  pause
